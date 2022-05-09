6ee5cc170d0d3502388796c5c0c450fba4cccb45
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2022-05-08 19:05:36 -0600

    Update Loadstring.lua

--- PresetFunctions/IsPlayerVisible/Loadstring.lua
+++ PresetFunctions/IsPlayerVisible/Loadstring.lua
@@ -1,3 +1,2 @@
 local IsPlayerVisible = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/PresetFunctions/IsPlayerVisible/Source.lua"))();
-
 -- Example: print(IsPlayerVisible(game.Players.EXAMPLENAME)) -- returns true or false based off if they are on your screen and able to be seen

125cbd6085a8c9ba6ea95ee12d9a6dde580e67b5
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2022-05-14 18:03:20 -0600

    Update SaveManager.lua

--- UI's/Linoria/SaveManager.lua
+++ UI's/Linoria/SaveManager.lua
@@ -182,7 +182,8 @@ local SaveManager = {} do
 	function SaveManager:BuildConfigSection(tab)
 		assert(self.Library, 'Must set SaveManager.Library')
 
-		local section = tab:AddRightGroupbox('Configuration')
+		local section1 = tab:AddRightGroupbox('Configuration')
+		local section = section1:AddTab("Configuration")
 
 		section:AddDropdown('SaveManager_ConfigList', { Text = 'Config list', Values = self:RefreshConfigList(), AllowNull = true })
 		section:AddInput('SaveManager_ConfigName',    { Text = 'Config name' })
